79c5b0da92fbcd58aad0ab5f515f8a8d53fbdd08
Priyanka saxena <saxena23priya@gmail.com>
2023-03-19 19:45:42 +0530

    'successful'

---
+++ Controllers/WeatherInfoController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileSystemGlobbing;
+using WeatherInfoApi.Models;
+
+namespace WeatherInfoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherInfoController : ControllerBase
+    {
+       List<Weather> weatherList;
+
+        public WeatherInfoController()
+        {
+            weatherList = new List<Weather>();
+            weatherList.Add(new Weather { Name="India",degree=40});
+            weatherList.Add(new Weather { Name = "Canada", degree = 10 });
+        }
+
+        [HttpGet]
+        public IActionResult Weatherinfo(string name1)
+        {
+            string name;
+            foreach(var weather in weatherList)
+            {
+                name = weather.Name;
+                if(name1 == name)
+                {
+                    return Ok(weather.degree);   
+                }
+                else if (name1 == weather.Name)
+                {
+                    return Ok(weather.degree);
+                }
+                
+
+            }
+
+            return Ok(20);
+        }
+
+       
+    }
+  }
---
+++ Models/Weather.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherInfoApi.Models
+{
+    public class Weather
+    {
+        //public int Id { get; set; }
+        public string? Name { get; set; }
+        public int degree { get; set; }
+    }
+}
